76ad2e052050b1c472ba2f770dc1462b3232d286
Xavier Mercado <xmercado1576@gmail.com>
2022-01-18 12:37:01 -0800

    feat(src): create components and images

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from '.Components/Header.js';
+import Footer from '.Components/Footer.js';
+import Portfolio from '.Components/Portfolio.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Header />
       </header>
+      <body>
+        <Portfolio />
+      </body>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
